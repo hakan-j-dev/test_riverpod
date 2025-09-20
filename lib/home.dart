a9aeaabf56d41882586edb53b1b3643a110c07a9
Hakan <hakan.j.dev@gmail.com>
2025-09-20 16:14:12 +0900

    ✨ first riverpod app 예시 구현

---
+++ lib/home.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:test_riverpod/joke.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Random Joke Generator')),
+      body: SizedBox.expand(
+        child: Consumer(
+          builder: (context, ref, child) {
+            final randomJoke = ref.watch(randomJokeProvider);
+
+            return Stack(
+              alignment: Alignment.center,
+              children: [
+                if (randomJoke.isRefreshing)
+                  const Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    child: LinearProgressIndicator(),
+                  ),
+                switch (randomJoke) {
+                  // When the request completes successfully, we display the joke.
+                  AsyncValue(:final value?) => SelectableText(
+                    '${value.setup}\n\n${value.punchline}',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 24),
+                  ),
+                  // On error, we display a simple error message.
+                  AsyncValue(error: != null) => const Text(
+                    'Error fetching joke',
+                  ),
+                  // While the request is loading, we display a progress indicator.
+                  AsyncValue() => const CircularProgressIndicator(),
+                },
+                Positioned(
+                  bottom: 20,
+                  child: ElevatedButton(
+                    onPressed: () => ref.invalidate(randomJokeProvider),
+                    child: const Text('Get another joke'),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-// 값을 저장할 "provider"를 생성합니다(여기서는 "Hello world").
-// provider를 사용하면 노출된 값을 모의(mock)//재정의(override)할 수 있습니다.
-final helloWorldProvider = Provider((_) => 'Hello, World!');
+import 'package:test_riverpod/home.dart';
 
 void main() {
-  // 위젯이 providers를 읽을 수 있게 하려면 전체 애플리케이션을 "ProviderScope" 위젯으로 감싸야 합니다.
-  // 여기에 providers의 상태가 저장됩니다.
-  runApp(ProviderScope(child: const MyApp()));
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-// Riverpod에 의해 노출되는 StatelessWidget 대신 ConsumerWidget을 확장합니다.
-class MyApp extends ConsumerWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final String value = ref.watch(helloWorldProvider);
-
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('example')),
-        body: Center(child: Text(value)),
-      ),
-    );
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomeView());
   }
 }
